b009aebc4c9020c04c8260caec3621f194e89212
Vaios96 <TsialkoutisV@gmail.com>
2023-06-16 13:20:07 +0300

    Chicko test v1.0

--- gr/aueb/cf4/elitebye/testing/Hello.java
+++ gr/aueb/cf4/elitebye/testing/Hello.java
@@ -2,6 +2,6 @@ package gr.aueb.cf4.elitebye.testing;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Hello Elite Byte CF4 team");
     }
 }
